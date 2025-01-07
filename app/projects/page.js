96e24aff4509e0845796d3f17ae674ebe43b5d9e
Charles Wefso <61127453+cwefso@users.noreply.github.com>
2025-01-07 12:06:32 -0700

    swap artistry link

--- app/projects/page.js
+++ app/projects/page.js
@@ -14,6 +14,34 @@ export default function Projects() {
           </Link>
         </span>
 
+<section>
+  <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+    <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+      <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+        <img
+          alt=""
+          src="/images/artist-try.png"
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+
+      <div className="lg:py-24">
+        <h1 className="text-3xl font-bold sm:text-4xl">ArtisTry</h1>
+
+        <p className="mt-4 text-white">
+       Discover classic artists and create your own gallery. This app uses an Express backend and was bootstrapped with Create React App.
+        </p>
+
+        <a
+          href="https://artistry-next.vercel.app/"
+          className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+        >
+          Check it out!
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
         <section>
   <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
     <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
@@ -74,34 +102,6 @@ export default function Projects() {
 
 
 
-<section>
-  <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-    <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-      <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
-        <img
-          alt=""
-          src="/images/ArtisTry.png"
-          className="absolute inset-0 h-full w-full object-cover"
-        />
-      </div>
-
-      <div className="lg:py-24">
-        <h1 className="text-3xl font-bold sm:text-4xl">ArtisTry</h1>
-
-        <p className="mt-4 text-white">
-       Discover classic artists and create your own gallery. This app uses an Express backend and was bootstrapped with Create React App.
-        </p>
-
-        <a
-          href="https://artis-try.vercel.app/"
-          className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
-        >
-          Check it out!
-        </a>
-      </div>
-    </div>
-  </div>
-</section>
 
     </main>
     </>
